5c708a5855ad26596cc573b5371a9bfebedc72d3
Alex <hsim1912@gmail.com>
2014-02-22 11:51:33 -0700

    started the generator and menu there for.  Have logic errors that need cleaning before this builds correctly.

--- Sprint2/tester.cpp
+++ Sprint2/tester.cpp
@@ -19,7 +19,6 @@
         o Take all input via the command line (cin)
 *******************************************************************************/  
 
-
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
@@ -31,6 +30,10 @@
 #include <time.h>
 #include <vector>
 #include <sstream>
+// QQQ!!! Alex : added
+#include <algorithm>
+#include <climits>
+#include <cstdlib>
 
 using namespace std;
 
@@ -39,6 +42,12 @@ string runtests(string progname, string specifictestcase);
 void writefinaloutfile(string progname, vector<string> finaloutfilecontents);
 vector<string> find_tsts(string progdir);
 int filesequal(string file1name, string file2name);
+void generatetestcases();
+void generatetestcasesmenu(bool &doubles, bool &lesserThanAmount, 
+                           bool &greaterThanAmount, double &min, double &max, 
+                           int &amountToGenerate, int &filesToMake);
+void solvetestcases();
+
 
 /*********************************** main ***********************************/
 //  Primary function of the program
@@ -52,7 +61,7 @@ int main(int argc, char* argv[])
   finaloutfilecontents.clear();
 
   //test for proper program usage from command line
-  if(argc < 2)
+  if(argc < 2) //QQQ!!! Alex: change to != 2
   {
     cout << "\nUsage:\ntester <source_file>\n Exiting.\n" << endl;
     return -1;
@@ -62,11 +71,30 @@ int main(int argc, char* argv[])
   prog_cpp = argv[1];
   progname = prog_cpp.substr(0,prog_cpp.find("."));
   progcomp = "g++ -o " + progname  + " " + prog_cpp;
-  size_t found = prog_cpp.find_last_of("/\\");
+  size_t found = prog_cpp.find_last_of("/\\");// QQQ!!! Alex : why not just ints?
   progdir = progname.substr(0,found+1);
   
   //compile program to be tested
   system(progcomp.c_str());
+
+  //QQQ!!! Alex: inserting here for new functionality
+  string ans;
+  do
+  {
+    cout << "Use only existing test cases?" << endl;
+    cin >> ans;
+    transform( ans.begin(), ans.end(), ans.begin(), ::tolower);
+    if (ans.compare("n") == 0 || ans.compare("no") == 0 )
+    {
+      generatetestcases();
+      break;
+    }
+    else if (ans.compare("y") == 0 || ans.compare("yes") == 0 )
+    {
+      break;
+    }
+  }while (1);
+
 
   //gathers all of the .tst files in current and sub directories of the program
   // being tested
@@ -100,6 +128,290 @@ int main(int argc, char* argv[])
 /********************************** END main **********************************/
 
 
+//QQQ!!! Alex : testcase builder starts here
+void generatetestcases()
+{
+  bool doubles, lesserThanAmount, greaterThanAmount;
+  double min, max;
+  long long inserts;
+  int amountToGenerate, filesToMake;
+  ofstream fout;
+  string file;
+  stringstream temp;
+  generatetestcasesmenu(doubles, lesserThanAmount, greaterThanAmount, min, 
+                        max, amountToGenerate, filesToMake);
+
+  // if max and min are both max (none selected, use 0,1 as it leaves rand)
+
+  // seed random
+  srand (time(NULL));
+
+  // if doubles
+  if (doubles)
+  {
+   // make each file
+    for (int i = 0 ; i < filesToMake; i +=1)
+    {
+      // make string of file name
+      temp.str("");
+      temp << i;
+      file = "GeneratedTestCase" + temp.str() + ".tst";
+      fout.open(file.c_str());
+
+      // populate 
+      if (lesserThanAmount)
+      {
+        inserts = rand() % amountToGenerate;
+        // if 0, run again
+        if (inserts == 0)
+        {
+          inserts = rand() % amountToGenerate;
+        }
+      }
+      else if (greaterThanAmount)
+      {
+        inserts = rand() % amountToGenerate * 5 + amountToGenerate;
+        // if amount... run again
+        if (inserts == amountToGenerate)
+        {
+          inserts = rand() % amountToGenerate * 5 + amountToGenerate;
+        }
+      }
+      else
+      {
+        inserts = amountToGenerate;
+      }
+      for (int j = 0; j < inserts; j +=1)
+      {
+        // threshold     offset to min   by random fraction   times the difference
+        double inValue = min + ((double) rand() / RAND_MAX) * (max - min);
+        fout << inValue << endl;
+      }
+
+      fout.close();
+    }
+  }
+
+  else
+  {
+    // make for ints
+    for (int i = 0 ; i < filesToMake; i +=1)
+    {
+      // make string of file name
+      temp.str("");
+      temp << i;
+      file = "GeneratedTestCase" + temp.str() + ".tst";
+      fout.open(file.c_str());
+      
+      // populate 
+      if (lesserThanAmount)
+      {
+        inserts = rand() % amountToGenerate;
+        // if 0, run again
+        if (inserts == 0)
+        {
+          inserts = rand() % amountToGenerate;
+        }
+      }
+      else if (greaterThanAmount)
+      {
+        inserts = rand() % amountToGenerate * 5 + amountToGenerate;
+        // if amount... run again
+        if (inserts == amountToGenerate)
+        {
+          inserts = rand() % amountToGenerate * 5 + amountToGenerate;
+        }
+      }
+      else
+      {
+        inserts = amountToGenerate;
+      }
+      // works for ints too
+      for (int j = 0; j < inserts; j +=1)
+      {
+        // threshold  offset to min   by random fraction   times the difference
+        int inValue = (int) (min + ((double) rand() / RAND_MAX) * (max - min));
+        fout << inValue << endl;
+      }
+
+      fout.close();
+    }
+
+  }
+
+}
+
+
+
+//QQQ!!! Alex : testcase builder menu is here... long...
+void generatetestcasesmenu(bool &doubles, bool &lesserThanAmount, 
+                           bool &greaterThanAmount, double &min, double &max, 
+                           int &amountToGenerate, int &filesToMake)
+{
+  string input = "";
+  bool range;
+
+  //make welcome menu
+  cout << "\nWelcome to the test-case generator!\n" << endl;
+  cout << "\033[1;34mbold Press 'x' and enter at any time to leave.\033[0m\n";
+
+  // start by asking about type
+  while(1)
+  {
+    cout << "What type of data would you like?  Ints or Doubles?"  << endl;
+    cin >> input;
+    transform( input.begin(), input.end(), input.begin(), ::tolower); 
+    if (!input.compare("int") || !input.compare("ints") || !input.compare("i"))
+    {
+      doubles = false;
+      break;
+    }   
+    else if (!input.compare("double") || !input.compare("doubles") 
+             || !input.compare("d"))
+    {
+      doubles = true;
+      break;
+    }
+    else if (!input.compare("x"))
+    {
+      return; // change of heart
+    }
+    else
+    {
+      cout << "\nI did not understand.  Please re-enter." << endl;
+    }
+  }
+
+  // count of test cases?
+  while(1)
+  {
+    cout << "How many test cases would you like?"  << endl;
+    cin >> input;
+    filesToMake = atoi(input.c_str());
+    if (filesToMake >= 0)
+    { 
+      break;
+    }
+    else if (!input.compare("x"))
+    {
+      return; // change of heart
+    }
+    else
+    {
+      cout << "\nPlease re-enter as a positive number or 'x' to cancel." << endl;
+    }
+  }
+
+  // specific amount to work with?
+  while(1)
+  {
+    cout << "Would you like a specific amount of items to test?"  << endl;
+    cin >> input;
+    transform( input.begin(), input.end(), input.begin(), ::tolower);
+    if (!input.compare("y") || !input.compare("yes"))
+    {
+      lesserThanAmount = greaterThanAmount = false;
+      break;
+    }
+    else if (!input.compare("n") || !input.compare("no"))
+    {
+      lesserThanAmount = greaterThanAmount = true;
+    }
+    else if (!input.compare("x"))
+    {
+      return; // change of heart
+    }
+    else
+    {
+      cout << "\nPlease re-enter." << endl;
+    }
+  }
+  
+  // if not
+  if (lesserThanAmount)
+  {
+    while(1)
+    {
+      cout << "More or less than a specific value?"  << endl;
+      cin >> input;
+      transform( input.begin(), input.end(), input.begin(), ::tolower);
+      if (!input.compare("<") || !input.compare("less"))
+      {
+        greaterThanAmount = false;
+        break;
+      }
+      else if (!input.compare(">") || !input.compare("more"))
+      {
+        lesserThanAmount = false;
+      }
+      else if (!input.compare("x"))
+      {
+        return; // change of heart
+      } 
+      else
+      {
+        cout << "\nPlease re-enter." << endl;
+      }
+    }
+  }
+
+  // value of items in tests
+  while(1)
+  {
+    cout << "What value should I work with?"  << endl;
+    cin >> input;
+    amountToGenerate = atoi(input.c_str());
+    if (amountToGenerate > 0)
+    {
+      break;
+    }
+    else if (!input.compare("x"))
+    {
+      return; // change of heart
+    }
+    else
+    {
+      cout << "\nPlease re-enter value greater than 0." << endl;
+    }
+  }
+
+  //range
+  do
+  {
+    cout << "range of values?" << endl;
+    cin >> input;
+    transform( input.begin(), input.end(), input.begin(), ::tolower);
+    if (input.compare("n") == 0 || input.compare("no") == 0 )
+    {
+      range = false;
+      break;
+    }
+    else if (input.compare("y") == 0 || input.compare("yes") == 0 )
+    {
+      break;
+    }
+  }
+  while (1);
+  
+  if (range)
+  {
+    cout << "What minimum value should I work with?"  << endl;
+    cin >> input;
+    min = atoi(input.c_str());
+
+    cout << "What max value should I work with?"  << endl;
+    cin >> input;
+    max = atoi(input.c_str());
+  }
+  else
+  {
+    // set min and max to max
+    min = LONG_MIN;
+    max = LONG_MAX; 
+  }
+}
+
+
 
 /********************************* find_tsts **********************************/
 // Locates all .tst files to be ran against the program to be tested
@@ -121,7 +433,7 @@ vector<string> find_tsts(string progdir)
     pclose( f );
     
     //removing that frustrating invisible character at the end of the strings
-    for(int i=0;i<tstfilelist.size();i++)
+    for(int i=0;i<tstfilelist.size();i++)// QQQ!!! Alex: clearer way to do this?
     {
       tstfilelist.at(i).replace(tstfilelist.at(i).end()-1,
       tstfilelist.at(i).end(),"");
@@ -180,13 +492,14 @@ string runtests(string progname, string specifictestcase)
 /********************************* filesequal *********************************/
 // compares two files and returns 1 if not equal, 0 if equal
 /******************************************************************************/ 
-int filesequal(string file1name, string file2name)
+int filesequal(string file1name, string file2name)  // QQQ!!! Alex: used as boolean,
+// so change return type or make clear that return is 0 for false (based on name)
 {
   ifstream file1, file2;
 
   //opening the two files to compare and ensuring they open properly
-  file1.open( file1name.c_str(), ios::in ); 
-  file2.open( file2name.c_str(), ios::in );
+  file1.open( file1name.c_str(), ios::in ); // QQQ!!! Alex: doesn't infile usualy
+  file2.open( file2name.c_str(), ios::in ); // defualt to in ios?
   if (!file1)
   {
     cout << "Couldn't open the file  " << file1name <<endl;
@@ -203,7 +516,7 @@ int filesequal(string file1name, string file2name)
   //read the two files into two string vectors
   vector<string> string1, string2;
   int i = 0;
-  while(file1.good())
+  while(file1.good())  // QQQ!!! Alex : change this to diff? or leave at io heavy?
   {
     string1.push_back("");
     getline(file1,string1.at(i)); 
